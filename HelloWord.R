bfac585897e42a3717869fb4d261a1eadc9084a8
JPow <js123powell@gmail.com>
2021-01-25 09:09:06 +0100

    Attempt 1 Jan 25 2021 to upload HelloWorld to the jeletti branched repo on my github profile.

---
+++ HelloWord.R
@@ -0,0 +1,6 @@
+# datasciencecoursera
+This is a Public Respository for completion of Coursera Data Toolbox Assignment #1
+
+This file outlines the various tasks in the Coursera Assignment
+
+## This is a markdown file 
